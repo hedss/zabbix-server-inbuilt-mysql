4b1d8a0eb1048180fbca6a21007bf3f4e2c577bf
Heds Simons <heds@whaleway.net>
2021-03-25 16:53:40 -0700

    Ensured sleep occurs on POLLHUP.
    Signed-off-by: Heds Simons <heds@whaleway.net>

--- entry.sh
+++ entry.sh
@@ -8,7 +8,10 @@ echo -e "${GREEN}Systemd init system enabled."
 # on startup once all other process have stopped.
 # We need this sleep to ensure this doesn't occur, else
 # logging to the console will not work.
+sleep infinity &
 for var in $(compgen -e); do
 	printf '%q=%q\n' "$var" "${!var}"
 done > /etc/docker.env
+echo -e "${GREEN}Envvars written, restarting systemd as PID1."
+
 exec /sbin/init
